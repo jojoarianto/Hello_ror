bc29b78865f8c9e95272d7a9ecbf8bb19dd9758d
jojoarianto <irianto94@outlook.com>
2016-05-29 13:36:51 +0700

    do create and read

---
+++ README.md
@@ -0,0 +1,1 @@
+# Say Hello Ruby On Rails
---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,28 @@
+class ContactsController < ApplicationController
+	def index
+		@contacts = Contact.all
+	end
+
+	def new
+	end
+
+	def create
+		@contact = Contact.new(contact_params)
+		@contact.save
+		redirect_to @contact
+	end
+
+	def show
+		@contact = Contact.find(params[:id])
+	end
+
+	def destroy
+		@contact = Contact.find(params[:id])
+		@contact.destroy
+	end
+
+	private
+		def contact_params
+			params.require(:contact).permit(:name, :nomor_hp)
+		end
+end
